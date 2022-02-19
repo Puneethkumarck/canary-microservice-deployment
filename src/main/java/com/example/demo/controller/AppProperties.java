fcfdc9e17869e0d0862c3819fd6e26fbc1691df5
PuneethKumarCk <puneethkumar.chikkakalya.kempanna@ing.com>
2022-02-20 00:35:40 +0100

    demo canary deployment

---
+++ src/main/java/com/example/demo/controller/AppProperties.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "greeting")
+public class AppProperties {
+
+    private String message;
+}
